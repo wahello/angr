266d6471ae85c8b533fa41c862e7eac507ccb803
Arvind <dnivra@users.noreply.github.com>
2020-04-08 19:49:30 -0700

    Removed few declarations from within case blocks - the variable is actually local to the function

--- native/sim_unicorn.cpp
+++ native/sim_unicorn.cpp
@@ -1204,7 +1204,6 @@ public:
 					{
 						taint_entity_t sink;
 						std::unordered_set<taint_entity_t> srcs;
-						block_taint_entry_t block_taint_entry;
 
 						sink.entity_type = TAINT_ENTITY_TMP;
 						sink.tmp_id = stmt->Ist.WrTmp.tmp;
@@ -1218,7 +1217,6 @@ public:
 					{
 						taint_entity_t sink;
 						std::unordered_set<taint_entity_t> srcs;
-						block_taint_entry_t block_taint_entry;
 
 						sink.entity_type = TAINT_ENTITY_MEM;
 						auto temp = get_taint_sources(stmt->Ist.Store.addr);
@@ -1233,7 +1231,6 @@ public:
 					{
 						taint_entity_t sink;
 						std::unordered_set<taint_entity_t> srcs;
-						block_taint_entry_t block_taint_entry;
 
 						sink.entity_type = TAINT_ENTITY_NONE;
 						srcs = get_taint_sources(stmt->Ist.Exit.guard);
